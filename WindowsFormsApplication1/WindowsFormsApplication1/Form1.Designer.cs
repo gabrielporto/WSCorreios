0ea288bb19c07b78d0dbc7c800e2daac103a8d00
Gabriel Porto Lopes <gabriel_porto_lopes@hotmail.com>
2017-03-12 12:39:15 -0300

    Primeiro Comit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskCep = new System.Windows.Forms.MaskedTextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskCep
+            // 
+            this.maskCep.Location = new System.Drawing.Point(85, 10);
+            this.maskCep.Mask = "00000-000";
+            this.maskCep.Name = "maskCep";
+            this.maskCep.Size = new System.Drawing.Size(70, 20);
+            this.maskCep.TabIndex = 0;
+            this.maskCep.KeyDown += new System.Windows.Forms.KeyEventHandler(this.maskCep_KeyDown);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(85, 36);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(70, 20);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 61);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.maskCep);
+            this.Name = "Form1";
+            this.Text = "Buscar Endereço";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskCep;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CorreiosWS ws = new CorreiosWS();
+
+            try
+            {
+                maskCep.Enabled = false;
+                btnBuscar.Enabled = false;
+                ws.ConsultaCEP(maskCep.Text);
+                MessageBox.Show(string.Format(@"Endereço: {0}
+                       Complemento 1: {1}
+                       Complemento 2: {2}
+                       Bairro: {3}
+                       Cidade: {4}
+                       Estado: {5}",
+                      ws.endERP.end,
+                      ws.endERP.complemento,
+                      ws.endERP.complemento2,
+                      ws.endERP.bairro,
+                      ws.endERP.cidade,
+                      ws.endERP.uf));
+
+                maskCep.Clear();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                maskCep.Enabled = true;
+                btnBuscar.Enabled = true;
+                maskCep.Select();
+            }
+        }
+
+        private void maskCep_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                maskCep.Enabled = false;
+                btnBuscar.Enabled = false;
+                button1_Click(sender, e);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskCep.Select();
+        }
+    }
+}
